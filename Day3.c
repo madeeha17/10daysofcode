8d65e61873405bf2200413f69d8132b12a5eb064
madeeha17 <85789189+madeeha17@users.noreply.github.com>
2021-11-27 20:35:22 +0530

    Add files via upload

---
+++ Day3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char str[100];
+  int i,len,flag=0;
+  printf("Enter the string:");
+  gets(str);
+  len=strlen(str);
+  for(i=0;i<len;i++)
+  {
+    if(str[i]!=str[len-i-1])
+    {
+      flag=1;
+      break;
+    }
+  }
+  if(flag==0)
+  {
+    printf ("%s is a palindrome",str);
+  }
+  else
+{
+  printf ("%s is not a palindrome",str);
+  return 0;
+}
+}
